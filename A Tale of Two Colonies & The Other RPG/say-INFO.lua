ab81c7d181ce4292d3b0d42847d7844bc8fef31e
Johnnyreyman <sytech@synolviaco.net>
2023-04-10 22:34:24 -0500

    Updated Infomaniac

--- A Tale of Two Colonies & The Other RPG/say-INFO.lua
+++ A Tale of Two Colonies & The Other RPG/say-INFO.lua
@@ -21,66 +21,169 @@ function displayMenu()
   end
 end
 
-local phrases = {
+local jokes = {
+  "Why did the creeper cross the road? To blow up StaticCurrent's house on the other side.",
+  "Why did the zombie refuse to attack StaticCurrent? He heard he was already dead.",
+  "What do you call a group of Minecraft players who refuse to use mods? Vanilla ice cream lovers.",
+  "Why did the Enderman refuse to teleport away with Cyano? He heard he was too heavy.",
+  "Why did the Ghast refuse to attack IvyKirkland? Because her builds are out of this world.",
+  "What's the best way to avoid a Creeper explosion? Stand behind IvyKirkland, she'll protect you with her shield.",
+  "Why did the Ghast refuse to attack Cyano? Because he found him too scary to face.",
+  "What's the easiest way to defeat a horde of zombies? Ask StaticCurrent, he has plenty of experience.",
+  "What do you call a group of Minecraft players who only build secret bases? The Undercover Squad.",
+  "What do you call a group of Minecraft players who never stops building? An architect."
+}
+  
+
+
+ local staticCurrentList = {
   "Remember God made you special and StaticCurrent by creepers shall be blown up.",
   "What was that noise?! Just Static being killed by another creeper.",
-  "MarTay607 angered the whole population of piglins, will she make the whole nether mad?",
-  "Cyano without his creative is like Trump once said, SAD.",
-  "IvyKirkland - Builds 50th iteration of Castle for the second time!",
-  "Cyano is so attractive that everyone wants a kiss, especially the creepers!",
   "StaticCurrent's house burns down from lightning. Ever heard of copper?",
-  "MarTay607 battles cute wilden beast and looses. What aren't they cuddly doggos?",
-  "Will StaticCurrent ever hope to discover how to make missiles for revenge on creepers? Nah.",
-  "Gold is necessary to live in the nether, take some and the piglins shall respect your authorata!",
-  "When in doubt, dig straight down like IvyKirkland.",
-  "Bring a bed to the nether and sleep on it. I dare you.",
-  "Cyano_SG is a horror to Smokey the Bear. Remember kids, only you can start forest fires!",
-  "Remember water buckets save hearts, or bunny slippers.",
-  "Don't waste diamonds on hoes, they're not worth it unless you're into that kind of thing.",
-  "Shields are like chastity belts, they keep the creepers out.",
   "Static goes fishin and gets a salmon. Cyano goes fishing in Minecraft and gets a boot.",
   "StaticCurrent cannot read a map. Usual.",
+  "StaticCurrent just can't help but die by creepers.",
+  "Why did the creeper cross the road? To blow up StaticCurrent's house on the other side.",
+  "Why did StaticCurrent stop using TNT? Because he realized it wasn't worth rebuilding his house every time it exploded.",
+  "Why did the zombie refuse to attack StaticCurrent? He heard he was already dead.",
+  "Why did the Ghast refuse to attack StaticCurrent? He heard he was already scared enough.",
+  "Why did the piglin refuse to trade with StaticCurrent? He had nothing to offer.",
+  "Why did the Ghast refuse to attack StaticCurrent? He heard he was already scared enough.",
+  "What's the easiest way to defeat a horde of zombies? Ask StaticCurrent, he has plenty of experience."
+}
+
+-- List of phrases related to Cyano
+
+ local cyanoList = {
+  "Cyano without his creative is like Trump once said, SAD.",
+  "Cyano_SG is a horror to Smokey the Bear. Remember kids, only you can start forest fires!",
+  "Don't waste diamonds on hoes, they're not worth it unless you're into that kind of thing.",
+  "Cyano is so attractive that everyone wants a kiss, especially the creepers!",
+  "Static goes fishin and gets a salmon. Cyano goes fishing in Minecraft and gets a boot.",
+  "Shields are like chastity belts, they keep the creepers out.",
   "Don't build buildings with tnt traps that look good. Cyano will light the fireplace and blow it up!",
   "Ivy and MarTay follow quest lines. Static and Cyano, 'What is this sorcery?'",
   "MarTay607 makes Secret Room! Cyano closed the door.",
-  "StaticCurrent just can't help but die by creepers.",
-  "Why did the creeper cross the road? To blow up StaticCurrent's house on the other side.",
   "If at first you don't succeed, call Cyano for creative mode.",
-  "What do you get when you put IvyKirkland and MarTay607 in a room together? A castle-building competition.",
-  "Why did the piglin refuse to trade with MarTay? Because he had no gold and a bad attitude.",
-  "What's the difference between StaticCurrent and a diamond pickaxe? The pickaxe is more durable.",
-  "Why did MarTay607 lose the battle against the wilden beast? She was too busy trying to pet it.",
-  "How many Creeper explosions does it take to destroy IvyKirkland's latest build? Just one, unfortunately.",
-  "Why did the Ender Dragon refuse to attack Cyano? Because he found him too scary to face.",
   "What do you call a group of Minecraft players who refuse to use mods? Vanilla ice cream lovers.",
-  "What's the easiest way to defeat a horde of zombies? Ask StaticCurrent, he has plenty of experience.",
-  "What do Minecraft players do after a long day of mining? They go to bedrock.",
-  "Why did the chicken cross the road in Minecraft? To escape the player chasing it with a sword.",
-  "Why did the Ghast refuse to attack IvyKirkland? Because her builds are out of this world.",
-  "What's the difference between MarTay607 and a beacon? The beacon shines brighter.",
-  "Why did StaticCurrent stop using TNT? Because he realized it wasn't worth rebuilding his house every time it exploded.",
   "What do you call a group of Minecraft players who only build secret bases? The Undercover Squad.",
   "Why did the skeleton invite Cyano to a party? He heard he was great at dancing (in real life).",
-  "What's the best way to avoid a Creeper explosion? Stand behind IvyKirkland, she'll protect you with her shield.",
-  "Why did MarTay607 refuse to enter the Nether? She heard it was too hot for her liking.",
-  "What do you call a Minecraft player who never dies? A legend, and definitely not StaticCurrent.",
-  "How many hearts does IvyKirkland have? Trick question - she doesn't need them because she's already immortal!",
-  "Why did the zombie refuse to attack StaticCurrent? He heard he was already dead.",
-  "What do you call a Minecraft player who never stops building? An architect.",
   "Why did the Enderman refuse to teleport away with Cyano? He heard he was too heavy.",
-  "What's the difference between MarTay607 and a creeper? MarTay won't blow up your house.",
-  "Why did the piglin refuse to trade with StaticCurrent? He had nothing to offer.",
-  "What do you call a Minecraft player who never gives up? A survivor.",
-  "Why did the Ender Dragon refuse to attack IvyKirkland? He heard she was too powerful.",
   "What's the difference between Cyano and a diamond pickaxe? Cyano is much sharper.",
-  "Why did the wilden beast refuse to attack MarTay? He heard she was too friendly.",
-  "What do you call a Minecraft player who never stops exploring? An adventurer.",
-  "Why did the Ghast refuse to attack StaticCurrent? He heard he was already scared enough.",
-  "What's the difference between IvyKirkland and a beacon? IvyKirkland is much brighter."
+  "Why did the Ghast refuse to attack Cyano? Because he found him too scary to face."
 }
+
+-- List of phrases related to IvyKirkland
+
+local ivyKirklandList = {
+  "IvyKirkland - Builds 50th iteration of Castle for the second time!",
+  "When in doubt, dig straight down like IvyKirkland.",
+  "Why did the Ghast refuse to attack IvyKirkland? Because her builds are out of this world.",
+  "What do you call a group of Minecraft players who never stops building? An architect.",
+  "What's the best way to avoid a Creeper explosion? Stand behind IvyKirkland, she'll protect you with her shield.",
+  "How many Creeper explosions does it take to destroy IvyKirkland's latest build? Just one, unfortunately."
+}
+
+ local martayList = {
+  "MarTay607 got lost again. Don't worry, he'll find his way back eventually.",
+  "Ivy and MarTay follow quest lines. Static and Cyano, 'What is this sorcery?'",
+  "Why did the Enderman refuse to teleport away with MarTay? He heard he was too heavy.",
+  "What's the difference between MarTay and a redstone circuit? One of them works consistently.",
+  "Why did the Ghast refuse to attack MarTay? Because he found him too charming to harm.",
+  "Why did the piglin refuse to trade with MarTay? He had nothing to offer.",
+  "Why did the skeleton invite MarTay to a party? He heard he was great at making music.",
+  "MarTay607 makes Secret Room! Cyano closed the door.",
+  "What do you call a group of Minecraft players who love to explore? The Adventurers' Guild.",
+}
+
+local mineRoastTriggers = "roast"
+local jokeTrigger = "tell me a joke"
+
+-- define a function to handle STATIC Roasts
+local function handleIncomingMessage(username, message, uuid, isHidden)
+  -- check if the message is directed at Bob
+  if message:match("Infomaniac") and message:match("Static") then
+    -- check if the message includes a trigger phrase to open the gate
+    for _, triggerPhrase in ipairs(mineRoastTriggers) do
+      if message:match(triggerPhrase) then
+        -- send a response to the user
+        local random_index = math.random(1, #staticCurrentList)
+        chatbox.sendMessage(staticCurrentList[random_index], "INFOMANIAC")
+      end
+    end
+  end
+end
+
+-- define a function to handle Ivy Roasts
+local function handleIncomingMessage(username, message, uuid, isHidden)
+  -- check if the message is directed at Bob
+  if message:match("Infomaniac") and message:match("Ivy") then
+    -- check if the message includes a trigger phrase to open the gate
+    for _, triggerPhrase in ipairs(mineRoastTriggers) do
+      if message:match(triggerPhrase) then
+        -- send a response to the user
+        local random_index = math.random(1, #ivyKirklandList)
+        chatbox.sendMessage(ivyKirklandList[random_index], "INFOMANIAC")
+      end
+    end
+  end
+end
+
+-- define a function to handle Cyano Roasts
+local function handleIncomingMessage(username, message, uuid, isHidden)
+  -- check if the message is directed at Bob
+  if message:match("Infomaniac") and message:match("Cyano") then
+    -- check if the message includes a trigger phrase to open the gate
+    for _, triggerPhrase in ipairs(mineRoastTriggers) do
+      if message:match(triggerPhrase) then
+        -- send a response to the user
+        local random_index = math.random(1, #cyanoList)
+        chatbox.sendMessage(cyanoList[random_index], "INFOMANIAC")
+      end
+    end
+  end
+end
+
+-- define a function to handle MarTay Roasts
+local function handleIncomingMessage(username, message, uuid, isHidden)
+  -- check if the message is directed at Bob
+  if message:match("Infomaniac") and message:match("MarTay") then
+    -- check if the message includes a trigger phrase to open the gate
+    for _, triggerPhrase in ipairs(mineRoastTriggers) do
+      if message:match(triggerPhrase) then
+        -- send a response to the user
+        local random_index = math.random(1, #martayList)
+        chatbox.sendMessage(martayList[random_index], "INFOMANIAC")
+      end
+    end
+  end
+end
+
+-- define a function to handle STATIC Roasts
+local function handleIncomingMessage(username, message, uuid, isHidden)
+  -- check if the message is directed at Bob
+  if message:match("Infomaniac") then
+    -- check if the message includes a trigger phrase to open the gate
+    for _, triggerPhrase in ipairs(jokeTrigger) do
+      if message:match(triggerPhrase) then
+        -- send a response to the user
+        local random_index = math.random(1, #jokes)
+        chatbox.sendMessageToPlayer(jokes[random_index], username, "INFOMANIAC")
+      end
+    end
+  end
+end
+
+
 
 -- Main program loop
 while true do
+   -- wait for the next chat message
+   local event, username, message, uuid, isHidden = os.pullEvent("chat")
+   -- handle the message if it exists
+   if username and message then
+     handleIncomingMessage(username, message, uuid, isHidden)
+   end
   -- Display menu and get user input
   displayMenu()
   local option = read()
